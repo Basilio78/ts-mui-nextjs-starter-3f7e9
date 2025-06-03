3600128cdc9a019db0cdee008e41f19fd7edb084
Basilio78 <ralonsof5@gmail.com>
2025-06-02 19:23:02 -0600

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,38 +1,23 @@
-import { ThemeProvider } from '@mui/material/styles';
-import CssBaseline from '@mui/material/CssBaseline';
-import { CacheProvider } from '@emotion/react';
-import theme from '../utils/theme';
-import createEmotionCache from '../utils/createEmotionCache';
-import Head from 'next/head';
+<Head>
+  <script
+    async
+    type="module"
+    src="https://interfaces.zapier.com/assets/webcomponents/zapier-interfaces-chatbot/zapier-interfaces-chatbot.js"
+  ></script>
+</Head>
 
-// Client-side cache, shared for the whole session of the user in the browser.
-const clientSideEmotionCache = createEmotionCache();
+...
 
-export default function App({ Component, pageProps, emotionCache = clientSideEmotionCache }: any) {
-  return (
-    <CacheProvider value={emotionCache}>
-      <Head>
-        {/* Script del chatbot de Zapier */}
-        <script
-          async
-          type="module"
-          src="https://interfaces.zapier.com/assets/webcomponents/zapier-interfaces-chatbot/zapier-interfaces-chatbot.js"
-        ></script>
-      </Head>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <Component {...pageProps} />
-        {/* Chatbot Norma embebido como pop-up */}
-        <div
-          dangerouslySetInnerHTML={{
-            __html: `
-              <zapier-interfaces-chatbot-embed
-                is-popup="true"
-                chatbot-id="cmb9jx6am0ohq0z6z6mbe9aiw">
-              </zapier-interfaces-chatbot-embed>
-            `,
-          }}
-        />
+<div
+  dangerouslySetInnerHTML={{
+    __html: `
+      <zapier-interfaces-chatbot-embed
+        is-popup="true"
+        chatbot-id="cmb9jx6am0ohq0z6z6mbe9aiw">
+      </zapier-interfaces-chatbot-embed>
+    `,
+  }}
+/>
       </ThemeProvider>
     </CacheProvider>
   );
